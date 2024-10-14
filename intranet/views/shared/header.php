24a1371af4326c4ba8018773bbb24ed2276308b6
MarienAngel16 <emary.presc16@gmail.com>
2024-10-14 14:00:02 -0600

    general  header design

--- intranet/views/shared/header.php
+++ intranet/views/shared/header.php
@@ -9,20 +9,4 @@
                 alt="Logo de la Escuela">
         </div>
         
-        <!-- Columna para la barra de navegación -->
-        <div class="col-lg-4 col-md-6 col-sm-12">
-            <nav class="navbar navbar-expand-lg navbar-light">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav ml-auto">
-                        <a class="nav-item nav-link active boton" href="#">Cuenta</a>
-                        <a class="nav-item nav-link boton" href="#">Dar de Alta</a>
-                    </div>
-                </div>
-            </nav>
-        </div>
-        
-    </div>
-</div>
+
